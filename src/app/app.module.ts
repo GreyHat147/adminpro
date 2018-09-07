39112b61ea628aa161ec5edf1fa97781f8c562c6
carlos rojas <carlos.rojas@servinformacion.com>
2018-09-07 16:57:47 -0500

    Creacion de modulo para manejar las pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
+
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
 import { APP_ROUTES } from './app.routes';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
 @NgModule({
@@ -19,13 +16,9 @@ import { RegisterComponent } from './login/register.component';
     AppComponent,
     LoginComponent,
     NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
     HeaderComponent,
     SidebarComponent,
     BreadcrumbComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
